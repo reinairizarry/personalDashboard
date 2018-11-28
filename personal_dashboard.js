e6a43e4affa12430925677bbad016dc5505b1934
Nicholas Murphy <147nrg@gmail.com>
2018-11-28 12:42:52 -0600

    added current temperature

--- personal_dashboard.js
+++ personal_dashboard.js
@@ -1,3 +1,27 @@
+var url = "http://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=3c59758cb4b64096ba0b4f377be9510b";
+var weather;
+
 function initApplication() {
   console.log("Initializing...");
+  getWeather();
+}
+function getWeather(){
+  var xhr = new XMLHttpRequest();
+  var value;
+  xhr.onreadystatechange = function() {
+  if (xhr.readyState==4 && xhr.status==200) {
+      value = xhr.responseText;
+      console.log("Collected weather info: " + value);
+      weather = JSON.parse(value);
+      document.getElementById("currentTemp").innerHTML = currentTempGreeting(weather.main.temp);
+    }
+  }
+  xhr.open("GET",url);
+  xhr.send();
+}
+function printTemp() {
+  console.log(weather.main.temp);
+}
+function currentTempGreeting(temp) {
+  return ("Greetings! The weather is "+ temp + " degrees.");
 }
