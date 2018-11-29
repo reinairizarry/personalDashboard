57ed4c79ecab6e051416e547204da4d7de4ae39e
Nicholas Murphy <147nrg@gmail.com>
2018-11-29 15:12:00 -0600

    testing create

--- createTodo.php
+++ createTodo.php
@@ -4,9 +4,11 @@
   //open and load JSON file
   $jsonString = file_get_contents('todo.json');
   $data = json_decode($jsonString,true);
-  $arr = array('title' => $title);
-  $newItem = json_encode($arr);
-  $data = array_pad($data,sizeof($data)+1,$newItem;
+  echo $data;
+  // $arr = array('title' => $title);
+  // $newItem = json_encode($arr);
+  // $data = array_pad($data,sizeof($data)+1,'';
+  // $data[sizeof($data)-1]
   $newJsonString = json_encode($data);
   echo("New JSON file: ".$newJsonString."\n");
   file_put_contents('todo.json', $newJsonString);
