49f63753526ae0500896cc19572d1f2d06362e1b
Nicholas Murphy <147nrg@gmail.com>
2018-11-29 15:36:22 -0600

    testing create

--- createTodo.php
+++ createTodo.php
@@ -3,13 +3,8 @@
   echo("Received item to add ".$title."\n");
   //open and load JSON file
   $jsonString = file_get_contents('todo.json');
-  echo $jsonString;
   $data = json_decode($jsonString,true);
-  echo "Loaded $size items\n";
-  $arr = array('title'=>$title);
-  $data += $arr;
-  //array_pad($data,($size+1),$arr); <-doesnt seem to do anything
-  //$data[$size+1] = $arr; <- breaks it by adding numbers
+  $data += array('title'=>$title);
   $newJsonString = json_encode($data);
   echo("New JSON file: ".$newJsonString."\n");
   file_put_contents('todo.json', $newJsonString);
