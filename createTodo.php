1103570650d4ec480107b62c97861bf9338ebd2e
Nicholas Murphy <147nrg@gmail.com>
2018-11-29 15:29:09 -0600

    testing create

--- createTodo.php
+++ createTodo.php
@@ -10,7 +10,8 @@
     echo "Loaded ".$data[$i]."\n";
   }
   $arr = array('title'=>$title);
-  array_pad($data,($size+1),$arr);
+  //array_pad($data,($size+1),$arr); <-doesnt seem to do anything
+  $data[$size+1] = $arr;
   $newJsonString = json_encode($data);
   echo("New JSON file: ".$newJsonString."\n");
   file_put_contents('todo.json', $newJsonString);
