94bf99169171b82cf816caa5293b7bae3bccb99d
Nicholas Murphy <147nrg@gmail.com>
2018-11-29 15:34:24 -0600

    testing create

--- createTodo.php
+++ createTodo.php
@@ -4,13 +4,13 @@
   //open and load JSON file
   $jsonString = file_get_contents('todo.json');
   echo $jsonString;
-  //$data = json_decode($jsonString,true);
-  $size = sizeof($data);
+  $data = json_decode($jsonString,true);
   echo "Loaded $size items\n";
   for($i;$i<$size;$i++){
     echo "Loaded ".$data[$i]."\n";
   }
   $arr = array('title'=>$title);
+  $data += $arr;
   //array_pad($data,($size+1),$arr); <-doesnt seem to do anything
   //$data[$size+1] = $arr; <- breaks it by adding numbers
   $newJsonString = json_encode($data);
